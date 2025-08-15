332dffefac298f9aabf21ec2ac07638abce5b500
whypowerd <whypowered@mail.com>
2025-08-15 16:54:27 -0400

    feat: link 'Join Community!' and 𝕏 to X community URL; size/spacing tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,42 @@ function App() {
       {/* Quantum Cat Animation - Full Screen */}
       <QuantumCat />
 
+      {/* Under-animation bar: left 'Join Community', right 'X' */}
+      <div className="w-[90vw] max-w-[600px] mx-auto -mt-3 flex items-center justify-start gap-10">
+        <DottedButton
+          text="Join Community!"
+          width={480}
+          height={108}
+          dotStep={5}
+          font="bold 80px Arial"
+          pointSize={2.4}
+          className="cursor-pointer"
+          onClick={() => {
+            const url = 'https://x.com/i/communities/1956338802244595784/';
+            window.open(url, '_blank', 'noopener,noreferrer');
+          }}
+          maxStaggerMs={1400}
+          approachFactor={0.07}
+          fadeInFactor={0.03}
+        />
+        <DottedButton
+          text="𝕏"
+          width={80}
+          height={108}
+          dotStep={4}
+          font="bold 84px Arial"
+          pointSize={2.6}
+          className="cursor-pointer"
+          onClick={() => {
+            const url = 'https://x.com/i/communities/1956338802244595784/';
+            window.open(url, '_blank', 'noopener,noreferrer');
+          }}
+          maxStaggerMs={600}
+          approachFactor={0.08}
+          fadeInFactor={0.04}
+        />
+      </div>
+
       <SignaturePortal />
     </div>
   );
